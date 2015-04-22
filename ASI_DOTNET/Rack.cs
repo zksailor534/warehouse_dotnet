9a433b4dd916554349ead828d4b096a388e1114e
zksailor534 <nate.greene@gmail.com>
2015-04-22 14:44:49 -0400

    Renamed namespace to ASI_DOTNET, isolated Utils to separate file

--- ASI_DOTNET/Rack.cs
+++ ASI_DOTNET/Rack.cs
@@ -11,33 +11,10 @@ using Autodesk.AutoCAD.Geometry;
 using Autodesk.AutoCAD.EditorInput;
 using Autodesk.AutoCAD.Windows;
 
-[assembly: CommandClass(typeof(ASI.Rack))]
+[assembly: CommandClass(typeof(ASI_DOTNET.Rack))]
 
-namespace ASI
+namespace ASI_DOTNET
 {
-    class Utils
-    {
-        public static Solid3d SweepPolylineOverLine(Database db,
-            Polyline p,
-            Line l)
-        {
-            /// Create a region from the polyline
-            DBObjectCollection acDBObjColl = new DBObjectCollection();
-            acDBObjColl.Add(p);
-            DBObjectCollection myRegionColl = new DBObjectCollection();
-            myRegionColl = Region.CreateFromCurves(acDBObjColl);
-            Region region = myRegionColl[0] as Region;
-
-            /// Create 3D solid and sweep existing region along path
-            Solid3d solSweep = new Solid3d();
-            SweepOptionsBuilder sob =
-                new SweepOptionsBuilder();
-            solSweep.CreateSweptSolid(region, l, sob.ToSweepOptions());
-
-            return solSweep;
-        }
-    }
-
     class Rack
     {
 
