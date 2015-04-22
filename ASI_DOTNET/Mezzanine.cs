a145c4538d793b3e10cca712222c544934307601
zksailor534 <nate.greene@gmail.com>
2015-04-22 15:59:55 -0400

    Updated CreateColumn to non-callable function; Added ColumnPrompt to call CreateColumn with User I/O

--- ASI_DOTNET/Mezzanine.cs
+++ ASI_DOTNET/Mezzanine.cs
@@ -17,13 +17,16 @@ namespace ASI_DOTNET
 {
     class Column
     {
-
-        [CommandMethod("CreateColumn")]
-        public static void CreateColumn()
+        public static void CreateColumn(Database acCurDb,
+            double cHeight,
+            double cDiameter,
+            double bDiameter)
         {
-            // Get the current document and database, and start a transaction
-            Document acDoc = Application.DocumentManager.MdiActiveDocument;
-            Database acCurDb = acDoc.Database;
+            // Frame block name
+            string cName = "Column - " + cHeight + " - " + cDiameter + "x" + cDiameter;
+
+            // Baseplate default height (could be argument)
+            double bHeight = 0.75;
 
             using (Transaction acTrans = acCurDb.TransactionManager.StartTransaction())
             {
@@ -32,119 +35,163 @@ namespace ASI_DOTNET
                 acBlkTbl = acTrans.GetObject(acCurDb.BlockTableId,
                                              OpenMode.ForRead) as BlockTable;
 
-                // Open the Block table record Model space for write
-                BlockTableRecord acBlkTblRec;
-                acBlkTblRec = acTrans.GetObject(acBlkTbl[BlockTableRecord.ModelSpace],
-                                                OpenMode.ForWrite) as BlockTableRecord;
+                // Blank objectid reference
+                ObjectId fId = ObjectId.Null;
 
-                // Prompt for the column height
-                PromptDoubleResult cHeightRes;
-                PromptDistanceOptions cHeightOpts = new PromptDistanceOptions("");
-                cHeightOpts.Message = "\nEnter the total column height: ";
-                cHeightRes = acDoc.Editor.GetDistance(cHeightOpts);
-                double cHeight = cHeightRes.Value;
-
-                // Exit if the user presses ESC or cancels the command
-                if (cHeightRes.Status == PromptStatus.Cancel) return;
-
-                // Prompt for the column width
-                PromptDoubleResult cWidthRes;
-                PromptDistanceOptions cWidthOpts = new PromptDistanceOptions("");
-                cWidthOpts.Message = "\nEnter the column width: ";
-                cWidthRes = acDoc.Editor.GetDistance(cWidthOpts);
-                double cWidth = cWidthRes.Value;
-
-                // Exit if the user presses ESC or cancels the command
-                if (cWidthRes.Status == PromptStatus.Cancel) return;
-
-                // Prompt for the base width
-                PromptDoubleResult bWidthRes;
-                PromptDistanceOptions bWidthOpts = new PromptDistanceOptions("");
-                bWidthOpts.Message = "\nEnter the baseplate width: ";
-                bWidthRes = acDoc.Editor.GetDistance(bWidthOpts);
-                double bWidth = bWidthRes.Value;
-
-                // Exit if the user presses ESC or cancels the command
-                if (bWidthRes.Status == PromptStatus.Cancel) return;
-
-                // Prompt for the base height
-                PromptDoubleResult bHeightRes;
-                PromptDistanceOptions bHeightOpts = new PromptDistanceOptions("");
-                bHeightOpts.Message = "\nEnter the baseplate height: ";
-                bHeightOpts.DefaultValue = 0.75;
-                bHeightRes = acDoc.Editor.GetDistance(bHeightOpts);
-                double bHeight = bHeightRes.Value;
-
-                // Exit if the user presses ESC or cancels the command
-                if (bHeightRes.Status == PromptStatus.Cancel) return;
-
-                // Prompt for the column orientation on the baseplate
-                PromptKeywordOptions cPlaceOpts = new PromptKeywordOptions("");
-                cPlaceOpts.Message = "\nEnter baseplate offset: ";
-                cPlaceOpts.Keywords.Add("Center");
-                cPlaceOpts.Keywords.Add("Side");
-                cPlaceOpts.Keywords.Add("Corner");
-                cPlaceOpts.Keywords.Default = "Center";
-                cPlaceOpts.AllowArbitraryInput = false;
-                PromptResult cPlaceRes = acDoc.Editor.GetKeywords(cPlaceOpts);
-
-                // Exit if the user presses ESC or cancels the command
-                if (cPlaceRes.Status == PromptStatus.Cancel) return;
-
-                // Calculate baseplate locations
-                Point3d bMid = new Point3d(bWidth / 2, bWidth / 2, bHeight / 2);
-                Vector3d bVec = Point3d.Origin.GetVectorTo(bMid);
-                var cBottom = cHeight / 2 + bHeight;
-                Vector3d cVec;
-                Point3d cMid;
-
-                // Calculate column location relative to baseplate
-                if (cPlaceRes.StringResult == "Corner")
+                if (acBlkTbl.Has(cName))
                 {
-                    cMid = new Point3d(cWidth / 2, cWidth / 2, cBottom);
-                }
-                else if (cPlaceRes.StringResult == "Side")
-                {
-                    cMid = new Point3d(bWidth / 2, cWidth / 2, cBottom);
+                    // Retrieve object id
+                    fId = acBlkTbl[cName];
                 }
                 else
                 {
-                    cMid = new Point3d(bWidth / 2, bWidth / 2, cBottom);
+                    using (BlockTableRecord acBlkTblRec = new BlockTableRecord())
+                    {
+                        acBlkTblRec.Name = cName;
+
+                        // Set the insertion point for the block
+                        acBlkTblRec.Origin = new Point3d(0, 0, 0);
+                        
+                        // Calculate baseplate locations
+                        Point3d bMid = new Point3d(bDiameter / 2, bDiameter / 2, bHeight / 2);
+                        Vector3d bVec = Point3d.Origin.GetVectorTo(bMid);
+                        var cBottom = cHeight / 2 + bHeight;
+                        Vector3d cVec;
+                        Point3d cMid;
+
+                        //// Calculate column location relative to baseplate
+                        //if (cPlaceRes.StringResult == "Corner")
+                        //{
+                        //    cMid = new Point3d(cWidth / 2, cWidth / 2, cBottom);
+                        //}
+                        //else if (cPlaceRes.StringResult == "Side")
+                        //{
+                        //    cMid = new Point3d(bWidth / 2, cWidth / 2, cBottom);
+                        //}
+                        //else
+                        //{
+                            cMid = new Point3d(bDiameter / 2, bDiameter / 2, cBottom);
+                        //}
+
+                        cVec = Point3d.Origin.GetVectorTo(cMid);
+                
+                        // Create the 3D solid baseplate
+                        Solid3d bPlate = new Solid3d();
+                        bPlate.SetDatabaseDefaults();
+                        bPlate.CreateBox(bDiameter, bDiameter, bHeight);
+
+                        // Position the baseplate 
+                        bPlate.TransformBy(Matrix3d.Displacement(bVec));
+
+                        // Add baseplate to block
+                        acBlkTblRec.AppendEntity(bPlate);
+
+                        // Create the 3D solid column
+                        Solid3d column = new Solid3d();
+                        column.SetDatabaseDefaults();
+                        column.CreateBox(cDiameter, cDiameter, cHeight - bHeight);
+
+                        // Position the column 
+                        column.TransformBy(Matrix3d.Displacement(cVec));
+
+                        // Add column to block
+                        acBlkTblRec.AppendEntity(column);
+
+                        /// Add Block to Block Table and close Transaction
+                        acBlkTbl.UpgradeOpen();
+                        acBlkTbl.Add(acBlkTblRec);
+                        acTrans.AddNewlyCreatedDBObject(acBlkTblRec, true);
+                    }
+
                 }
 
-                cVec = Point3d.Origin.GetVectorTo(cMid);
-                
-                // Create the 3D solid baseplate
-                Solid3d b1 = new Solid3d();
-                b1.SetDatabaseDefaults();
-                b1.CreateBox(bWidth, bWidth, bHeight);
-
-                // Position the baseplate 
-                b1.TransformBy(Matrix3d.Displacement(bVec));
-
-                // Create the 3D solid column
-                Solid3d c1 = new Solid3d();
-                c1.SetDatabaseDefaults();
-                c1.CreateBox(cWidth, cWidth, cHeight);
-
-                // Position the column 
-                c1.TransformBy(Matrix3d.Displacement(cVec));
-
-                // Add the new objects to the block table record and the transaction
-                acBlkTblRec.AppendEntity(b1);
-                acTrans.AddNewlyCreatedDBObject(b1, true);
-                acBlkTblRec.AppendEntity(c1);
-                acTrans.AddNewlyCreatedDBObject(c1, true);
-
-                // Open the active viewport
-                ViewportTableRecord acVportTblRec;
-                acVportTblRec = acTrans.GetObject(acDoc.Editor.ActiveViewportId,
-                                                  OpenMode.ForWrite) as ViewportTableRecord;
-
-                // Save the new objects to the database
+                // Save the new object to the database
                 acTrans.Commit();
+
             }
+
         }
+
+
+        [CommandMethod("ColumnPrompt")]
+        public static void ColumnPrompt()
+        {
+            // Frame default characteristics
+            double cHeight = 96;
+            double cDiameter = 6;
+            double bDiameter = 16;
+
+            // Get the current document and database, and start a transaction
+            // !!! Move this outside function
+            Document acDoc = Application.DocumentManager.MdiActiveDocument;
+            Database acCurDb = acDoc.Database;
+
+            // Prompt for the column height
+            PromptDoubleResult cHeightRes;
+            PromptDistanceOptions cHeightOpts = new PromptDistanceOptions("");
+            cHeightOpts.Message = "\nEnter the total column height: ";
+            cHeightRes = acDoc.Editor.GetDistance(cHeightOpts);
+            cHeight = cHeightRes.Value;
+
+            // Exit if the user presses ESC or cancels the command
+            if (cHeightRes.Status == PromptStatus.Cancel) return;
+
+            // Prompt for the column width
+            PromptDoubleResult cDiameterRes;
+            PromptDistanceOptions cDiameterOpts = new PromptDistanceOptions("");
+            cDiameterOpts.Message = "\nEnter the column diameter: ";
+            cDiameterRes = acDoc.Editor.GetDistance(cDiameterOpts);
+            cDiameter = cDiameterRes.Value;
+
+            // Exit if the user presses ESC or cancels the command
+            if (cDiameterRes.Status == PromptStatus.Cancel) return;
+
+            // Prompt for the base width
+            PromptDoubleResult bDiameterRes;
+            PromptDistanceOptions bDiameterOpts = new PromptDistanceOptions("");
+            bDiameterOpts.Message = "\nEnter the baseplate diameter: ";
+            bDiameterRes = acDoc.Editor.GetDistance(bDiameterOpts);
+            bDiameter = bDiameterRes.Value;
+
+            // Exit if the user presses ESC or cancels the command
+            if (bDiameterRes.Status == PromptStatus.Cancel) return;
+
+            //// Prompt for the base height
+            //PromptDoubleResult bHeightRes;
+            //PromptDistanceOptions bHeightOpts = new PromptDistanceOptions("");
+            //bHeightOpts.Message = "\nEnter the baseplate height: ";
+            //bHeightOpts.DefaultValue = 0.75;
+            //bHeightRes = acDoc.Editor.GetDistance(bHeightOpts);
+            //double bHeight = bHeightRes.Value;
+
+            //// Exit if the user presses ESC or cancels the command
+            //if (bHeightRes.Status == PromptStatus.Cancel) return;
+
+            //// Prompt for the column orientation on the baseplate
+            //PromptKeywordOptions cPlaceOpts = new PromptKeywordOptions("");
+            //cPlaceOpts.Message = "\nEnter baseplate offset: ";
+            //cPlaceOpts.Keywords.Add("Center");
+            //cPlaceOpts.Keywords.Add("Side");
+            //cPlaceOpts.Keywords.Add("Corner");
+            //cPlaceOpts.Keywords.Default = "Center";
+            //cPlaceOpts.AllowArbitraryInput = false;
+            //PromptResult cPlaceRes = acDoc.Editor.GetKeywords(cPlaceOpts);
+
+            //// Exit if the user presses ESC or cancels the command
+            //if (cPlaceRes.Status == PromptStatus.Cancel) return;
+
+            Column.CreateColumn(acCurDb, cHeight, cDiameter, bDiameter);
+
+            //// Open the active viewport
+            //ViewportTableRecord acVportTblRec;
+            //acVportTblRec = acTrans.GetObject(acDoc.Editor.ActiveViewportId,
+            //                                  OpenMode.ForWrite) as ViewportTableRecord;
+
+            //// Save the new objects to the database
+            //acTrans.Commit();
+
+        }
+
     }
     
 }
