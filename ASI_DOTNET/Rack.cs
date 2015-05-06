71d9fbf8c0dd38da94802dcac61ccc919812ab01
zksailor534 <nate.greene@gmail.com>
2015-05-06 10:07:51 -0400

    Initial release of Beam class (new structure) Frame and Truss to be moved to class structure in later commit

--- ASI_DOTNET/Rack.cs
+++ ASI_DOTNET/Rack.cs
@@ -11,13 +11,13 @@ using Autodesk.AutoCAD.Geometry;
 using Autodesk.AutoCAD.EditorInput;
 using Autodesk.AutoCAD.Windows;
 
-[assembly: CommandClass(typeof(ASI_DOTNET.Rack))]
+[assembly: CommandClass(typeof(ASI_DOTNET.Frame))]
+[assembly: CommandClass(typeof(ASI_DOTNET.Beam))]
 
 namespace ASI_DOTNET
 {
-    class Rack
+    class Frame
     {
-
         public static void CreateFrame(Database acCurDb,
             double fHeight,
             double fWidth,
@@ -164,9 +164,8 @@ namespace ASI_DOTNET
 
         }
 
-
-        [CommandMethod("RackPrompt")]
-        public static void RackPrompt()
+        [CommandMethod("FramePrompt")]
+        public static void FramePrompt()
         {
             // Get the current document and database, and start a transaction
             // !!! Move this outside function
@@ -207,9 +206,259 @@ namespace ASI_DOTNET
             if (diameterRes.Status != PromptStatus.OK) return;
 
             // Create Frame block
-            Rack.CreateFrame(acCurDb, height, width, diameter);
+            Frame.CreateFrame(acCurDb, height, width, diameter);
         }
 
+    }
+
+    class Beam
+    {
+        // Auto-impl class properties
+        private Database db;
+        public double length { get; private set; }
+        public double height { get; private set; }
+        public double width { get; private set; }
+        public string name { get; private set; }
+        public string orientation { get; private set; }
+        public string style { get; private set; }
+
+        // Public constructor
+        public Beam (Database db,
+            double length,
+            double height = 3,
+            double width = 2,
+            string orient = "X-Axis",
+            string style = "Step")
+        {
+            this.db = db;
+            this.length = length;
+            this.height = height;
+            this.width = width;
+            this.orientation = orient;
+            this.style = style;
+            this.name = "Rack Beam - " + length + "x" + height;
+        }
+
+        public void Build()
+        {
+            using (Transaction acTrans = db.TransactionManager.StartTransaction())
+            {
+                // Open the Block table for read
+                BlockTable acBlkTbl;
+                acBlkTbl = acTrans.GetObject(db.BlockTableId, OpenMode.ForRead) as BlockTable;
+
+                // Blank objectid reference
+                ObjectId fId = ObjectId.Null;
+
+                if (acBlkTbl.Has(name))
+                {
+                    // Retrieve object id
+                    fId = acBlkTbl[name];
+                }
+                else
+                {
+                    // Create new block (record)
+                    using (BlockTableRecord acBlkTblRec = new BlockTableRecord())
+                    {
+                        acBlkTblRec.Name = name;
+
+                        // Set the insertion point for the block
+                        acBlkTblRec.Origin = new Point3d(0, 0, 0);
+
+                        // Create beam profile
+                        Polyline beamPoly = beamProfile(height,
+                            width,
+                            style: style,
+                            orient: orientation);
+
+                        // Create beam path
+                        Line beamLine = beamPath(length, orientation);
+
+                        // Create beam
+                        Solid3d beamSolid = Utils.SweepPolylineOverLine(beamPoly, beamLine);
+
+                        acBlkTblRec.AppendEntity(beamSolid);
+
+                        /// Add Block to Block Table and close Transaction
+                        acBlkTbl.UpgradeOpen();
+                        acBlkTbl.Add(acBlkTblRec);
+                        acTrans.AddNewlyCreatedDBObject(acBlkTblRec, true);
+                    }
+
+                }
+                // Save the new object to the database
+                acTrans.Commit();
+            }
+        }
+
+        // Draw beam profile
+        private Polyline beamProfile (double height,
+            double width,
+            string style = "Step",
+            double stepSize = 0.75,
+            string orient = "X-Axis")
+        {
+            int i = 0;
+
+            /// Create polyline of brace profile and rotate to correct orienation
+            Polyline poly = new Polyline();
+            poly.AddVertexAt(i, new Point2d(0, 0), 0, 0, 0);
+            poly.AddVertexAt(i++, new Point2d(-height, 0), 0, 0, 0);
+
+            // Add vertices for beam shape
+            if (style == "Step")
+            {
+                poly.AddVertexAt(i++, new Point2d(-height, width - stepSize), 0, 0, 0);
+                poly.AddVertexAt(i++, new Point2d(-height + stepSize, width - stepSize), 0, 0, 0);
+                poly.AddVertexAt(i++, new Point2d(-height + stepSize, width), 0, 0, 0);
+            }
+            else if (style == "Box")
+            {
+                poly.AddVertexAt(i++, new Point2d(-height, width), 0, 0, 0);
+            }
+
+            poly.AddVertexAt(i++, new Point2d(0, width), 0, 0, 0);
+            poly.Closed = true;
+            poly.TransformBy(Matrix3d.Rotation(Math.PI / 2, Vector3d.YAxis, Point3d.Origin));
+
+            // Rotate beam profile if necessary
+            if (orient == "Y-Axis")
+            {
+                poly.TransformBy(Matrix3d.Rotation(Math.PI / 2, Vector3d.ZAxis, Point3d.Origin));
+            }
+
+            return poly;
+        }
+
+        private Line beamPath (double length,
+            string orient)
+        {
+            Line path;
+
+            if (orient == "Y-Axis")
+            {
+                path = new Line(Point3d.Origin, new Point3d(0, length, 0));
+            }
+            else // X-Axis (default)
+            {
+                path = new Line(Point3d.Origin, new Point3d(length, 0, 0));
+            }
+
+            return path;
+        }
+
+        [CommandMethod("BeamPrompt")]
+        public static void BeamPrompt()
+        {
+            // Get the current document and database, and start a transaction
+            // !!! Move this outside function
+            Document acDoc = Application.DocumentManager.MdiActiveDocument;
+            Database acCurDb = acDoc.Database;
+
+            // Prepare prompt for the beam length
+            PromptDoubleResult lengthRes;
+            PromptDistanceOptions lengthOpts = new PromptDistanceOptions("");
+            lengthOpts.Message = "\nEnter the beam length: ";
+            lengthOpts.DefaultValue = 96;
+
+            // Prepare prompt for the beam height
+            PromptDoubleResult heightRes;
+            PromptDistanceOptions heightOpts = new PromptDistanceOptions("");
+            heightOpts.Message = "\nEnter the beam height: ";
+            heightOpts.DefaultValue = 3;
+
+            // Prepare prompt for the beam width
+            PromptDoubleResult widthRes;
+            PromptDistanceOptions widthOpts = new PromptDistanceOptions("");
+            widthOpts.Message = "\nEnter the beam width: ";
+            widthOpts.DefaultValue = 2;
+
+            // Prepare prompt for other options
+            PromptResult bOthersRes;
+            PromptKeywordOptions bOthersOpts = new PromptKeywordOptions("");
+            bOthersOpts.Message = "\nBeam Options: ";
+            bOthersOpts.Keywords.Add("Orientation");
+            bOthersOpts.Keywords.Add("Style");
+            bOthersOpts.AllowArbitraryInput = false;
+            bOthersOpts.AllowNone = true;
+
+            // Prepare prompt for the beam orientation
+            string orientation = "X-Axis";
+            PromptResult bOrientRes;
+            PromptKeywordOptions bOrientOpts = new PromptKeywordOptions("");
+            bOrientOpts.Message = "\nEnter beam orientation: ";
+            bOrientOpts.Keywords.Add("X-Axis");
+            bOrientOpts.Keywords.Add("Y-Axis");
+            bOrientOpts.Keywords.Default = "X-Axis";
+            bOrientOpts.AllowArbitraryInput = false;
+
+            // Prepare prompt for the beam style
+            string style = "Step";
+            PromptResult bStyleRes;
+            PromptKeywordOptions bStyleOpts = new PromptKeywordOptions("");
+            bStyleOpts.Message = "\nEnter beam style: ";
+            bStyleOpts.Keywords.Add("Step");
+            bStyleOpts.Keywords.Add("Box");
+            bStyleOpts.Keywords.Default = "Step";
+            bStyleOpts.AllowArbitraryInput = false;
+
+            // Prompt for beam length
+            lengthRes = acDoc.Editor.GetDistance(lengthOpts);
+            if (lengthRes.Status != PromptStatus.OK) return;
+            double length = lengthRes.Value;
+
+            // Prompt for beam height
+            heightRes = acDoc.Editor.GetDistance(heightOpts);
+            if (heightRes.Status != PromptStatus.OK) return;
+            double height = heightRes.Value;
+
+            // Prompt for beam width
+            widthRes = acDoc.Editor.GetDistance(widthOpts);
+            if (widthRes.Status != PromptStatus.OK) return;
+            double width = widthRes.Value;
+            
+            // Prompt for other options
+            bOthersRes = acDoc.Editor.GetKeywords(bOthersOpts);
+
+            // Exit if the user presses ESC or cancels the command
+            if (bOthersRes.Status == PromptStatus.Cancel) return;
+
+            while (bOthersRes.Status == PromptStatus.OK)
+            {
+                switch (bOthersRes.StringResult)
+                {
+                    case "Orientation":
+                        bOrientRes = acDoc.Editor.GetKeywords(bOrientOpts);
+                        orientation = bOrientRes.StringResult;
+                        if (bOrientRes.Status != PromptStatus.OK) return;
+                        break;
+                    case "Style":
+                        bStyleRes = acDoc.Editor.GetKeywords(bStyleOpts);
+                        style = bStyleRes.StringResult;
+                        if (bStyleRes.Status == PromptStatus.Cancel) return;
+                        break;
+                    default:
+                        Application.ShowAlertDialog("Invalid Keyword");
+                        break;
+                }
+
+                // Re-prompt for keywords
+                bOthersRes = acDoc.Editor.GetKeywords(bOthersOpts);
+
+                // Exit if the user presses ESC or cancels the command
+                if (bOthersRes.Status == PromptStatus.Cancel) return;
+            }
+
+            // Create beam
+            Beam rackBeam = new Beam(db: acCurDb,
+                length: length,
+                height: height,
+                width: width,
+                orient: orientation,
+                style: style);
+            rackBeam.Build();
+        }
+                        
     }
 
 }
